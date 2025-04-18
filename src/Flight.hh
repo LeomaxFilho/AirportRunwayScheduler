a35d4b2263d9b976b232c9f8dde333fb5da3fd98
Leomax <leomax.filho@gmail.com>
2025-04-18 15:02:09 -0300

    Add: Delete.cc | Fix: bestFilght tiebreak criterion

--- src/Flight.hh
+++ src/Flight.hh
@@ -182,7 +182,7 @@ int Flight::bestFlight(int idxPrevious, int currentTime){
                 }
                 if ((totalFee(i, idxPrevious, currentTime) == totalFee(bestFlight, idxPrevious, currentTime)))
                 {
-                    if (timeToFlight[i] < timeToFlight[bestFlight])
+                    if (fee[i] < fee[bestFlight])
                     {
                         bestFlight = i;
                     }
@@ -206,53 +206,6 @@ void Flight::addTime(int whichRunway, int idxCurrent, int idxPrevious){
     }
     runways[whichRunway].second = departureTime[idxCurrent] + timeToFlight[idxCurrent];
 }
-
-/*
-void Flight::bestRunway() {
-    // Limpa as pistas antes de alocar os voos
-    for (int i = 0; i < numberOfRunWays; ++i) {
-        runways[i].first.clear(); // Limpa o vetor da pista
-        runways[i].second = 0; // Reinicializa o tempo da pista
-    }
-    
-    
-    haveFlown.assign(numberOfFlights, false); // Reinicializa o vetor haveFlown
-
-    // Aloca os voos para as pistas
-    for (int i = 0; i < numberOfFlights; ++i) {
-        int whichRunway = 0;
-        for (int j = 0; j < numberOfRunWays; ++j) {
-            if (runways[j].second < runways[whichRunway].second) {
-                whichRunway = j;
-            }
-        }
-
-        int bestIdx = bestFlight(runways[whichRunway].first.empty() ? -1 : runways[whichRunway].first.back(), runways[whichRunway].second);
-        runways[whichRunway].first.push_back(bestIdx); // Adiciona o voo ao vetor da pista
-        
-        // Verifica se a pista está vazia antes de passar o voo anterior para addTime
-        int previousFlight = -1;
-        if (!runways[whichRunway].first.empty()) {
-            if (runways[whichRunway].first.size() > 1) {
-                previousFlight = runways[whichRunway].first[runways[whichRunway].first.size() - 2];
-            }
-        }
-        addTime(whichRunway, bestIdx, previousFlight);
-
-        haveFlown[bestIdx] = true;
-    }
-
-    // Exibe a alocação dos voos nas pistas
-    std::cout << "\nAlocacao dos voos nas pistas:" << std::endl;
-    for (int i = 0; i < numberOfRunWays; ++i) {
-        std::cout << "Pista " << i << ": ";
-        for (int flightIdx : runways[i].first) {
-            std::cout << flightIdx << " ";
-        }
-        cout << " tempo - " << runways[i].second;
-        std::cout << std::endl;
-    }
-}*/
 
 void Flight::bestRunway() {
     // Limpa as pistas antes de alocar os voos
