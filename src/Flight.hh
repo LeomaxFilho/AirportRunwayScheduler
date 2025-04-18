4a610898ee8fbb56e496c44bce74d22752188b13
Luis Alves <luisnetoalemanha@gmail.com>
2025-04-17 22:16:55 -0300

    Problema da leitura do arquivo

---
+++ src/Flight.hh
@@ -0,0 +1,289 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <list>
+#include <fstream>
+#include <queue>
+#include <utility>
+
+using namespace std;
+
+class Flight
+{
+private:
+    int numberOfFlights;
+    int numberOfRunWays;
+    vector<pair<vector<int>, int>> runways; // Da pra melhorar essa estrutura
+    vector<bool> haveFlown;
+    vector<int> departureTime;
+    vector<int> timeToFlight;
+    vector<int> fee ;
+    vector<vector<int>> wakeTurbulence;
+
+public:
+
+    Flight(string file);
+    Flight();
+    ~Flight();
+    int totalFee(int idxCurrent, int idxPrevious, int time);
+    int bestFlight(int idxPrevious, int currentTime);
+    void bestRunway ();
+    void addTime(int whichRunway, int idxCurrent, int idxPrevious);
+    void showInputs();
+
+};
+
+/**
+* TODO - Deve ser implementado um construtor para que seja possível adicionar o arquivo
+* TODO - diretamente do arquivo de texte de **input**
+*/ 
+
+Flight::Flight(){}
+
+void Flight::showInputs(){
+    cout << numberOfFlights << endl;
+    cout<< numberOfRunWays << endl << endl;
+
+    // Vetor de tempos de partida
+    for(int i=0; i< numberOfFlights; i++){
+        cout << departureTime[i] << " ";
+    }
+    cout << endl << endl;
+    // Vetor de tempo de partida/chegada
+    for(int i=0; i< numberOfFlights; i++){    
+        cout << timeToFlight[i] << " ";
+    }        
+
+    cout << endl << endl;
+
+    for(int i=0; i< numberOfFlights; i++){    
+        cout << fee[i] << " ";
+    }    
+
+    cout << endl << endl;
+
+
+    // Matriz de tempos
+    for(int i=0; i< numberOfFlights; i++){
+        for(int j=0; j<numberOfFlights; j++){
+            cout << wakeTurbulence[i][j] << " ";
+        }  
+        cout << endl;
+    }
+}
+
+
+Flight::Flight(string file){
+    
+    ifstream readFile(file);
+    int temp;
+
+    cout << file << endl;
+
+    if(!readFile.is_open()){
+        cout << "Erro ao abrir o arquivo: " <<  file << endl;
+        exit(EXIT_FAILURE); 
+    }
+    
+    readFile >> numberOfFlights;
+    readFile >> numberOfRunWays;
+
+    // Vetor de tempos de partida
+    for(int i=0; i< numberOfFlights; i++){
+        readFile >> temp;
+        departureTime.push_back(temp);
+    }
+    // Vetor de tempo de partida/chegada
+    for(int i=0; i< numberOfFlights; i++){
+        readFile >> temp;
+        timeToFlight.push_back(temp);
+    }
+    // Vetor de multa para cada voo
+    for(int i=0; i< numberOfFlights; i++){
+        readFile >> temp;
+        fee.push_back(temp);
+    }
+    
+    // TA QUEBRANDO AQUIIIIIIIII
+    // Matriz de tempos
+    for(int i=0; i <  numberOfFlights; i++){
+        for(int j=0; j < numberOfFlights; j++){
+            readFile >> temp;
+            cout << temp << endl;
+            wakeTurbulence[i].push_back(temp);
+        }  
+        cout << endl;
+    }
+    cout << "aquiiiiiiiiiiiii\n";    
+    
+    readFile.close();
+    
+    // Inicializacao do Bitmask todos com zero
+    for(int i=0; i< numberOfFlights; i++){
+        haveFlown.push_back(0);
+    }
+}
+
+Flight::~Flight(){
+
+}
+
+int Flight::totalFee(int idxCurrent, int idxPrevious, int currentTime){
+    int time = (currentTime + wakeTurbulence[idxPrevious][idxCurrent] - departureTime[idxCurrent]);
+    return time * fee[idxCurrent];
+}
+
+int Flight::bestFlight(int idxPrevious, int currentTime){
+
+    int bestFlight = -1;
+    for (int i = 0; i < numberOfFlights; ++i) {
+        if (!haveFlown[i]) {
+            bestFlight = i;
+            break;
+        }
+    }
+    if (bestFlight == -1)
+    {
+        return -1;
+    }
+    
+    if (idxPrevious == -1)
+    {
+        for (int i = 0; i < numberOfFlights; i++)
+        {
+            if ((bestFlight != i) && (!haveFlown[i]))
+            {
+                if (departureTime[i] < departureTime[bestFlight])
+                    bestFlight = i;
+                if (departureTime[bestFlight] == departureTime[i])
+                {
+                    if (timeToFlight[i] < timeToFlight[bestFlight])
+                    {
+                        bestFlight = i;
+                    }
+                }
+            }
+        }
+    }else{
+        for (int i = 0; i < numberOfFlights; i++)
+        {
+            if ((bestFlight != i) && (!haveFlown[i]))
+            {
+                if (totalFee(i, idxPrevious, currentTime) > totalFee(bestFlight, idxPrevious, currentTime))
+                {
+                    bestFlight = i;
+                }
+                if ((totalFee(i, idxPrevious, currentTime) == totalFee(bestFlight, idxPrevious, currentTime)))
+                {
+                    if (timeToFlight[i] < timeToFlight[bestFlight])
+                    {
+                        bestFlight = i;
+                    }
+                }
+            }
+        }
+    }
+    return bestFlight;
+}
+
+void Flight::addTime(int whichRunway, int idxCurrent, int idxPrevious){
+    if (idxPrevious != -1)
+    {
+        if ((runways[whichRunway].second + wakeTurbulence[idxPrevious][idxCurrent])  < departureTime[idxCurrent])
+        {
+            runways[whichRunway].second = departureTime[idxCurrent] + timeToFlight[idxCurrent];
+            return;
+        }
+        runways[whichRunway].second += wakeTurbulence[idxPrevious][idxCurrent] + timeToFlight[idxCurrent];
+        return;
+    }
+    runways[whichRunway].second = departureTime[idxCurrent] + timeToFlight[idxCurrent];
+}
+
+
+void Flight::bestRunway() {
+    // Limpa as pistas antes de alocar os voos
+    for (int i = 0; i < numberOfRunWays; ++i) {
+        runways[i].first.clear();
+        runways[i].second = 0; // Reinicializa o tempo da pista
+    }
+
+    haveFlown.assign(numberOfFlights, false); // Reinicializa o vetor haveFlown
+
+    // Aloca os voos para as pistas
+    for (int i = 0; i < numberOfFlights; ++i) {
+        int whichRunway = 0;
+        for (int j = 0; j < numberOfRunWays; ++j) {
+            if (runways[j].second < runways[whichRunway].second) {
+                whichRunway = j;
+            }
+        }
+
+        int bestIdx = bestFlight(runways[whichRunway].first.empty() ? -1 : runways[whichRunway].first.back(), runways[whichRunway].second);
+        runways[whichRunway].first.push_back(bestIdx); // Adiciona o voo ao vetor da pista
+        
+        // Verifica se a pista está vazia antes de passar o voo anterior para addTime
+        int previousFlight = -1;
+        if (!runways[whichRunway].first.empty()) {
+            if (runways[whichRunway].first.size() > 1) {
+                previousFlight = runways[whichRunway].first[runways[whichRunway].first.size() - 2];
+            }
+        }
+        addTime(whichRunway, bestIdx, previousFlight);
+
+        haveFlown[bestIdx] = true;
+    }
+
+    // Exibe a alocação dos voos nas pistas
+    cout << "Alocação dos voos nas pistas:" << std::endl;
+    for (int i = 0; i < numberOfRunWays; ++i) {
+        cout << "Pista " << i << ": ";
+        for (int flightIdx : runways[i].first) {
+            cout << flightIdx << " ";
+        }
+        cout << endl;
+    }
+}
+
+
+/*
+// TODO adicionar o critério para usar a pista que esta livre primeiro sempre..
+// TODO algo como olhar o tempo atual de cada uma delas e decidir qual delas é a melhor para o momento
+void Flight::bestRunway (){
+
+    int bestIdx = 0;
+    int whichRunway = 0;
+
+    for (int i = 0; i < numberOfFlights; i++)
+    {
+        for (int j = 0; j < numberOfRunWays; j++)
+        {
+            if (runways[j].second < runways[whichRunway].second)
+            {
+                whichRunway = j;
+            }
+        }
+        bestIdx = bestFlight(runways[whichRunway].first.back(), runways[whichRunway].second);
+        addTime(whichRunway, bestIdx, runways[whichRunway].first.back());
+        haveFlown[bestIdx] = true;
+    }
+}*/
+//#endif !FLIGHT
+
+/**
+    whichRunway = i % numberOfRunWays;
+    if (runways[whichRunway].first.empty())
+    {
+        bestIdx = bestFlight(-1, 0);
+        runways[whichRunway].first.push(bestIdx);
+        addTime(whichRunway, bestIdx, -1);
+        haveFlown[bestIdx] = true;
+        continue;
+    }
+    bestIdx = bestFlight(runways[whichRunway].first.back(), runways[whichRunway].second);
+    runways[whichRunway].first.push(bestIdx);
+    addTime(whichRunway, bestIdx, runways[whichRunway].first.back());
+    haveFlown[bestIdx] = true;
+    continue;
+ */
---
+++ src/main.cc
@@ -0,0 +1,34 @@
+#include "Flight.hh"
+
+/*int greedy(int numberOfFlights, int numberOfRunways, Flight flights, std::vector<std::vector<int>> wakeTurbulence,
+std::vector<std::queue<int>>& runways){
+        
+    std::vector<int> time(2);
+    for (int i = 0; i < 2; i++)
+        time[i] = 0;
+
+    for (int i = 0; i < numberOfFlights; i++)
+    {
+
+        
+    }
+    
+        
+
+}*/
+
+
+int main(int argc, char *argv[]) {
+
+    //std::cout << "Nome da instancia chamada: " << argv[1] << std::endl;
+
+    Flight flights("../inputs/teste.txt");
+
+    cout << "passou aqui\n";
+    flights.showInputs();
+    
+    //flights.bestRunway();
+
+
+    return 0;
+}
